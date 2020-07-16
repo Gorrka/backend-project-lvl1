5b3fa0c4656f5736c72008d281011dd8eb33b8b2
Gorrka <46000290+Gorrka@users.noreply.github.com>
2020-07-16 13:25:14 +0000

    Fix some

--- src/games/even.js
+++ src/games/even.js
@@ -1,8 +1,6 @@
 import { gameBasis, gameLimit } from '../index.js';
 
-
 const task = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-
 
 const engineGame = () => {
   // задаем необходимые переменные
@@ -10,9 +8,10 @@ const engineGame = () => {
   const gameData = [];
   // запускаем цикл
   for (let counter = 0; counter < gameLimit; counter += 1) {
-    const getRandomInRange = Math.floor(Math.random() * Math.floor(maxNumber));// повторяется в 2 играх
+    const getRandomInRange = Math.floor(Math.random() * Math.floor(maxNumber));
+    const question = `${getRandomInRange}`;
     const rightAnswer = (getRandomInRange % 2 === 0) ? 'yes' : 'no';
-    gameData.push([question, rightAnswer.toString()]);//
+    gameData.push([question, rightAnswer.toString()]);
   }
   return gameData;
 };
