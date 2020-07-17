5e501c20a634c437c4a6d4a2b7248a9627b635ea
Gorrka <46000290+Gorrka@users.noreply.github.com>
2020-07-17 14:11:00 +0000

    Add gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runTheGcdGame from '../src/games/gcd.js';
+
+runTheGcdGame();
---
+++ src/games/gcd.js
@@ -0,0 +1,28 @@
+import { gameBasis, gameLimit } from '../index.js';
+import getRandomNumber from '../utility.js';
+
+const task = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (y, x) => {
+  if (y > x) return getGcd(y, x);
+  if (!y) return x;
+  return NOD(y, x % y);
+};
+
+const engineGame = () => {
+  // задаем необходимые переменные
+  const maxNumber = 100;
+  const gameData = [];
+  // запускаем цикл
+  for (let counter = 0; counter < gameLimit; counter += 1) {
+    const randomNumber1 = getRandomNumber(maxNumber);
+    const randomNumber2 = getRandomNumber(maxNumber);
+    const question = `${randomNumber1} ${randomNumber2`;
+    const rightAnswer = getGcd(randomNumber1, randomNumber2);
+    gameData.push([question, rightAnswer.toString()]);
+  }
+  return gameData;
+};
+const runTheGcdGame = () => gameBasis(task, engineGame);
+
+export default runTheGcdGame;
