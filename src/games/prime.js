c74259fd6288782cc895a7db78b7e0306a9d0272
Gorrka <46000290+Gorrka@users.noreply.github.com>
2020-07-20 13:25:13 +0000

    Missing {

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,8 +4,9 @@ import { getRandomNumber } from '../utility.js';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1)
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
+  }
   return num > 1;
 };
 
