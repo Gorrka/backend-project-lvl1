ac576148e99d4977372cd3c2fd3ab3c4baef1017
Gorrka <46000290+Gorrka@users.noreply.github.com>
2020-07-20 12:39:49 +0000

    fix problem

--- src/games/progression.js
+++ src/games/progression.js
@@ -13,14 +13,14 @@ const engineGame = () => {
     const firstRandomNumber = getRandomNumber(maxNumber);
     const lastRandomNumber = getRandomNumber(maxNumber);
     const progressionSize = 10;
-    const hiddenNumberOfProgression = randomInRange(0, progressionSize - 1);// ?????
+    const hiddenNumberOfProgression = randomInRange(0, progressionSize - 1);
 
     for (let j = 0; j < progressionSize; j += 1) {
       progression[j] = firstRandomNumber + lastRandomNumber * j;
     }
-    const question = progression.join(' ');
     const rightAnswer = progression[hiddenNumberOfProgression];
     progression[hiddenNumberOfProgression] = '..';
+    const question = progression.join(' ');
     gameData.push([question, rightAnswer.toString()]);
   }
   return gameData;
@@ -28,3 +28,30 @@ const engineGame = () => {
 const runTheProgressionGame = () => gameBasis(task, engineGame);
 
 export default runTheProgressionGame;
+
+/*
+const task = 'What number is missing in the progression?';
+
+const collectGameData = () => {
+  const gameData = [];
+  for (let i = 0; i < limitOfAnswers; i += 1) {
+    const progression = [];
+    const firstProgressionNumber = randomInteger();
+    const difference = randomInteger();
+    const progressionSize = 9;
+    const huddenNumberIndex = randomInteger(0, progressionSize - 1);
+    for (let j = 0; j < progressionSize; j += 1) {
+      progression[j] = firstProgressionNumber + difference * j;
+    }
+    const answer = progression[huddenNumberIndex];
+    progression[huddenNumberIndex] = '..';
+    const question = progression.join(' ');
+    gameData.push([question, answer.toString()]);
+  }
+  return gameData;
+};
+
+const startProgressionGame = () => game(task, collectGameData);
+
+export default startProgressionGame;
+*/
