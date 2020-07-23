6cb6f1df385a366b7fc90b82f7d9eef47f501cd9
Gorrka <geomerida@mail.ru>
2020-07-24 00:06:19 +0300

    PPC

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 #!/usr/bin/env node
 import runTheEvenGame from '../src/games/even.js';
-=======
-#!/usr/bin/env node
-import runTheEvenGame from '../src/games/even.js';
->>>>>>> 878e2a6ea5cf28bc767fbd07485fc41acb20d53a
 
 runTheEvenGame();
